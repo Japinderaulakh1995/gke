3bc4ef9511d43730e52ba678976fe251e1155ab3
Nanu <112574952+Japinderaulakh1995@users.noreply.github.com>
2024-12-30 18:04:49 +0530

    Create networking.tf

---
+++ terraform/networking.tf
@@ -0,0 +1,71 @@
+locals {
+  enabled_apis = [
+    "serviceusage.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "container.googleapis.com",
+    "gkehub.googleapis.com" 
+  ]
+}
+resource "google_project_service" "enabled_apis" {
+  for_each           = toset(local.enabled_apis)
+  service            = each.value
+  disable_on_destroy = false
+  project            = var.project_name
+}
+
+**************************************************
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "${var.project_name}-vpc"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = "${var.project_name}-subnet"
+  ip_cidr_range = var.gke_node_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "pods-subnet"
+    ip_cidr_range = var.pods_cidr
+  }
+  secondary_ip_range {
+    range_name    = "services-subnet"
+    ip_cidr_range = var.svc_cidr
+  }
+}
+*****************************************************
+resource "google_compute_router" "nat_router" {
+  name    = "${var.project_name}-router"
+  network = google_compute_network.vpc_network.id
+  region  = var.region
+}
+
+resource "google_compute_address" "global_ip" {
+  name         = "${var.project_name}-global-ip"
+  address_type = "EXTERNAL"
+  region       = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.project_name}-nat"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.global_ip.self_link]
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+*******************************************************************
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "${var.project_name}-allow-ssh"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
