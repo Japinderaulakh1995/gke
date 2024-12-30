43672963d6287fc4baf6cc6b1fafcfcf2cbc9290
Nanu <112574952+Japinderaulakh1995@users.noreply.github.com>
2024-12-30 18:03:41 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,3 @@
-locals {
-    labels = {
-        "project_name" = var.project_name
-    }
-}
-
 variable "project" {
     type= string
     description = "ID Google project"
@@ -17,4 +11,23 @@ variable "region" {
 variable  "project_name" {
     type = string
     description = "Name of data pipeline project to use as resource prefix"
+}
+variable "gke_node_cidr" {
+  description = "cidr range for nodes"
+  default = "10.0.0.0/18"
+}
+
+variable "pods_cidr" {
+  description = "cidr range for pod"
+  default = "10.48.0.0/14"
+}
+
+variable "svc_cidr" {
+  description = "cidr range for service"
+  default = "10.52.0.0/20"
+}
+
+variable "master_ipv4_cidr_block" {
+  description = "cidr range for service"
+  default = "172.16.0.0/28"
 }
