af12b3a9f97c59caf87a240cfabf2665282b50cd
Nanu <112574952+Japinderaulakh1995@users.noreply.github.com>
2024-12-30 18:05:14 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,8 +1,56 @@
-resource "google_storage_bucket" "raw" {
-  project = var.project
-  name = "${var.project_name}-rawwww"
-  force_destroy = false
-  uniform_bucket_level_access = true
-  location = var.region
-  labels = local.labels
+resource "google_container_cluster" "primary" {
+  name               =  "${var.project_name}-cluster"
+  location           = var.region
+  initial_node_count = 1
+  deletion_protection = false
+  network            = google_compute_network.vpc_network.name
+  subnetwork         = google_compute_subnetwork.subnetwork.name
+ip_allocation_policy {
+    cluster_secondary_range_name  = "pods-subnet"
+    services_secondary_range_name = "services-subnet"
+  }
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes    = true
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
+  }
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+    network_policy_config {
+      disabled = false
+    }
+  }
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+}
+*************************************************************************
+resource "google_container_node_pool" "general" {
+  name       ="${var.project_name}-node-pool"
+  cluster    = google_container_cluster.primary.id
+  node_count = 2
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+  
+   autoscaling {
+    min_node_count = 2
+    max_node_count = 4
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "f1-micro"
+
+    service_account =  "gke-terrafrom@sacred-epigram-446218-g5.iam.gserviceaccount.com"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
