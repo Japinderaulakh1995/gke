c96c996507fd50df26886d1415e2088bf43e67b2
Nanu <112574952+Japinderaulakh1995@users.noreply.github.com>
2025-01-12 19:25:12 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name               =  "${var.project_name}-cluster"
-  location           = var.region
+  location           = "us-central1-a"
   initial_node_count = 1
   remove_default_node_pool = true
   deletion_protection = false
@@ -52,6 +52,11 @@ resource "google_container_node_pool" "general" {
     service_account =  "gke-terrafrom@sacred-epigram-446218-g5.iam.gserviceaccount.com"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
     ]
   }
 }
