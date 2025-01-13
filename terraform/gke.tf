139b8d9bbbfc6c5335ea737fad912a874d121402
Nanu <112574952+Japinderaulakh1995@users.noreply.github.com>
2025-01-13 23:58:58 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -12,7 +12,7 @@ ip_allocation_policy {
   }
   private_cluster_config {
     enable_private_endpoint = false
-    enable_private_nodes    = true
+    enable_private_nodes    = false
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
   }
   addons_config {
